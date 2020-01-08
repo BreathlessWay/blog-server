2eda429a7f1ea4a5c4e8ee5a457be7a3374bf639
BreathlessWay <731005087@qq.com>
2020-01-08 16:13:37 +0800

    fix: 标签bug

--- app/service/tag.ts
+++ app/service/tag.ts
@@ -27,62 +27,64 @@ export default class TagService extends Service {
 		const { ctx } = this;
 
 		if (!list.length) {
-			return ctx.model.Tag.deleteMany({});
-		}
+			await ctx.model.Tag.deleteMany({});
+		} else {
+			const updateList: Array<Partial<TagItemType>> = [],
+				addList: Array<Omit<TagItemType, '_id'>> = [];
+			let deleteList: Array<Partial<TagItemType>> = [];
 
-		const updateList: Array<Partial<TagItemType>> = [],
-			addList: Array<Omit<TagItemType, '_id'>> = [];
-		let deleteList: Array<Partial<TagItemType>> = [];
-
-		const listResult = (await ctx.model.Tag.find()) || [];
-		list.forEach(item => {
-			if (item._id) {
-				listResult.forEach(t => {
-					if (t._id.toString() === item._id) {
-						updateList.push(item);
-					}
-				});
-			} else {
-				addList.push(item);
+			const listResult = (await ctx.model.Tag.find()) || [];
+			list.forEach(item => {
+				if (item._id) {
+					listResult.forEach(t => {
+						if (t._id.toString() === item._id) {
+							updateList.push(item);
+						}
+					});
+				} else {
+					addList.push(item);
+				}
+			});
+			const updateListIds = updateList.map(item => item._id);
+			if (listResult && listResult.length) {
+				deleteList = listResult.filter(
+					item =>
+						!updateListIds.includes(item._id.toString()) &&
+						!item.article.length,
+				);
 			}
-		});
-		const updateListIds = updateList.map(item => item._id);
-		if (listResult && listResult.length) {
-			deleteList = listResult.filter(
-				item =>
-					!updateListIds.includes(item._id.toString()) && !item.article.length,
-			);
+			const updateBatch = updateList.map(item => {
+				return {
+					updateOne: {
+						filter: { _id: item._id },
+						update: item,
+					},
+				};
+			});
+
+			const deleteBatch = deleteList.map(item => {
+				return {
+					deleteOne: {
+						filter: { _id: item._id },
+					},
+				};
+			});
+
+			const addBatch = addList.map(item => {
+				return {
+					insertOne: {
+						document: item,
+					},
+				};
+			});
+
+			await ctx.model.Tag.bulkWrite([
+				...updateBatch,
+				...deleteBatch,
+				...addBatch,
+			]);
 		}
-		const updateBatch = updateList.map(item => {
-			return {
-				updateOne: {
-					filter: { _id: item._id },
-					update: item,
-				},
-			};
-		});
 
-		const deleteBatch = deleteList.map(item => {
-			return {
-				deleteOne: {
-					filter: { _id: item._id },
-				},
-			};
-		});
-
-		const addBatch = addList.map(item => {
-			return {
-				insertOne: {
-					document: item,
-				},
-			};
-		});
-
-		await ctx.model.Tag.bulkWrite([
-			...updateBatch,
-			...deleteBatch,
-			...addBatch,
-		]);
 		return this.getTagList();
 	}
 }
